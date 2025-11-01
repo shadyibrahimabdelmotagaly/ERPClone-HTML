515e27cf6cd24116d2dbb4883deb4b7b56737e5e
shadyibrahimabdelmotagaly <shaadyibrahimabdelmotagaly@gmail.com>
2025-11-02 01:29:06 +0200

    ERPClone-GPT TEST 7
    GPT TEST7

--- jsmain.js
+++ jsmain.js
@@ -1,16 +1,14 @@
 function goToPage(page) {
-    // أنيميشن خروج
     document.body.style.opacity = '0';
-    document.body.style.transition = 'opacity 0.3s';
-
     setTimeout(() => {
-        // الانتقال للصفحة
         window.location.href = page;
     }, 300);
 }
 
-// عند تحميل أي صفحة
 window.addEventListener('load', () => {
     document.body.style.opacity = '1';
-    document.body.style.transition = 'opacity 0.5s';
 });
+
+function goBack() {
+    goToPage('../index.html');
+}
